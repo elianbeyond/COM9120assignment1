f0d4c67be513e217f2f4f33a63f8b1bc27cdfa87
Bingxuan Cao <814846181@qq.com>
2022-09-17 15:49:50 +0800

    只看src下面的三个文件即可，方便修改

---
+++ MyUtils/ListNode.java
@@ -0,0 +1,36 @@
+package JAVA.MyUtils;
+
+public  class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }//Leetcode中链表的定义
+
+
+    private static ListNode createLinkedList(int[] arr) {//将输入的数组输入到链表中
+        if (arr.length == 0) {
+            return null;
+        }
+        ListNode head = new ListNode(arr[0]);
+        ListNode current = head;
+        for (int i = 1; i < arr.length; i++) {//过程
+            current.next = new ListNode(arr[i]);
+            current = current.next;
+        }
+        return head;
+    }
+
+    private static void printLinkedList(ListNode head) {//将链表结果打印
+        ListNode current = head;
+        while (current != null) {
+            System.out.printf("%d -> ", current.val);
+            current = current.next;
+        }
+        System.out.println("NULL");
+    }
+
+
+
+}
---
+++ src/JAVA/LCTest.java
@@ -0,0 +1,24 @@
+package JAVA;
+
+import java.util.*;
+public class LCTest {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+
+
+    }
+}
+
+/**
+ *
+ */
+class Solution {
+    public void validateStackSequences(int[] pushed, int[] popped) {
+        int len1 = pushed.length;
+        int len2 = popped.length;
+        return ;
+
+    }
+}
